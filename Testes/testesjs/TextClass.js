29a3692cca3c36b5d95c43ac3db30845f0243cd2
Kauã Alves <kauaAlvesWorkplace@gmail.com>
2021-09-10 19:18:37 -0300

    10/09/21

---
+++ Testes/testesjs/TextClass.js
@@ -0,0 +1,25 @@
+
+// aparentemente não funciona assim sobrecarga em js ;-;
+/* 
+class TextingFeatures{
+
+    constructor(number1, number2){
+        this.number1 = number1
+        this.number2 = number2 
+    }
+    constructor(number1, number2, hipotenusa){
+        this.number1 = number1
+        this.number2 = number2
+        this.hipotenusa = hipotenusa
+    }
+
+    calculatePerimeter(number1, number2, number3){
+        return number1 + number2 + number3
+    }
+
+    addNumbers(number1, number2){
+        return number1 + number2
+    }
+
+}
+*/
