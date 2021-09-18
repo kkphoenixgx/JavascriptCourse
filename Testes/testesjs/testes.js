12c1bd99a271d86eda4db8e33d90b33cef0abeaa
Kauã Alves <kauaAlvesWorkplace@gmail.com>
2021-09-18 19:12:32 -0300

    18/09/21

--- Testes/testesjs/testes.js
+++ Testes/testesjs/testes.js
@@ -1,3 +1,16 @@
+//Indicie
+/*
+
+10. Json
+28.Factory
+
+*/
+
+
+
+//----------------------------Json----------------------------
+
+/*
 const screen = document.querySelector("#screen");
 
 Json = {
@@ -10,3 +23,42 @@ Json.number2 = 2;
 
 console.log(Json);
 screen.innerHTML = Json.number1 + Json.number2;
+
+*/
+
+// --------------------------Factory--------------------------
+
+/*
+function FabricPeople(nome, sobrenome){
+    pessoa = {}
+    pessoa.nome = nome;
+    pessoa.sobrenome = sobrenome;
+
+    return pessoa
+}
+var Kauã = FabricPeople("Kauã", "Alves")
+console.log(Kauã);
+
+function FabricCars(model, year){
+
+    Car = {}
+    Car.model = model; 
+    Car.year= year;
+
+    function modelAndYear(){
+        return `Model: ${Car.model}, Year: ${Car.year}`
+    }
+
+    Car.modelAndYear = modelAndYear();
+
+    return Car
+
+}
+
+var Carro1 = FabricCars("Reno", "euSeiLaPorra")
+
+console.log(Carro1);
+console.log(Carro1.model);
+console.log(Carro1.modelAndYear);
+
+*/
