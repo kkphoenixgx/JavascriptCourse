745b18f82545b321270b218498cf1d67936a9409
Kauã Alves <kauaAlvesWorkplace@gmail.com>
2021-10-04 11:19:46 -0300

    04/10/21

--- Projetos/NodeJsScripts/serverExemple.js
+++ Projetos/NodeJsScripts/serverExemple.js
@@ -1,23 +1,37 @@
 const http = require('http');
-// importando o modulo para a const http
 
 let server = http.createServer((req, res) => {
 
     console.log('URL: ', req.url);
     console.log("Method: ", req.method);
 
-    req.end("ok")
-});
+    switch (req.url) {
+        case '/':
+            
+            res.statusCode = 200;
+            res.setHeader('content-type', 'text/html')
+            res.end('<h1>Hello</h1>')
 
-/*
-    criando o servidor e dizendo o que ele precisa fazer 
-    ao iniciar o servidor
-*/
+            break;
+    
+        case '/users':
+
+            res.statusCode = 200;
+            res.setHeader('content-type', 'application/json')
+            res.end(JSON.stringify({
+                users:[{
+                    user: "Kauã Alves",
+                    email : "macacodeoculos123@gmail.com",
+                    id: 1
+                }]}
+            ));
+
+            break;
+    }
+});
 
 server.listen(3000, "127.0.0.1", ()=>{
 
     console.log("Servidor rodando");
 
 });
-
-/* dizendo onde o servidor precisa escutar */
