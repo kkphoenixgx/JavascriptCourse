b70ee7b0049b956a5b3c9f7eb84dbd3ca0378e32
Kauã Alves <kauaAlvesWorkplace@gmail.com>
2021-10-31 01:50:23 -0300

    30/10/21 work
    There is some Issues to fix but that is the main files to start the project tomorrow

--- Projetos/ProjetoClientServer/ServerRESTful/routes/pathIndex.js
+++ Projetos/ProjetoClientServer/ServerRESTful/routes/pathIndex.js
@@ -1,4 +1,4 @@
-module.exports = app=>{
+module.exports = app => {
         
     app.get('/', (req, res) => {
 
@@ -8,4 +8,4 @@ module.exports = app=>{
 
     });
 
-} 
+};
--- Projetos/ProjetoClientServer/ServerRESTful/serverIndex.js
+++ Projetos/ProjetoClientServer/ServerRESTful/serverIndex.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const consign = require('consign');
 const bodyParser = require('body-parser');
+const expressValidator = require('express-validator')
 
 var app = express();
 app.use(bodyParser.urlencoded( { extended: false } ));
 app.use(bodyParser.json());
-
+app.use(expressValidator());
 
 consign().include('routes').include('Utils').into(app);
 
