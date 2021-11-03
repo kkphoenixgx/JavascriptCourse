f381cd22cb95015e858b9c43dab975821310e7f5
Kauã Alves <kauaAlvesWorkplace@gmail.com>
2021-11-02 21:13:41 -0300

    02/11/21

--- Projetos/ProjetoClientServer/clientServer/routes/users.js
+++ Projetos/ProjetoClientServer/clientServer/routes/users.js
@@ -1,9 +1,22 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+const restify = request('restify-client');
+const assert = request('assert');
+
+// create a json client for restify
+var client = restify.createJsonCLient({
+    url: 'http//localhost:4000'
+})
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  
+  client.get('/users', function(err, restifyReq, restReq, object){
+    assert.ifError(err);
+    res.end(JSON.stringify(object, null, 2))
+  });
+
 });
 
 module.exports = router;
