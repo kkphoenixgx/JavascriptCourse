75804a500869f0768797cd9268e49a5850073138
Kauã Alves <kauaAlvesWorkplace@gmail.com>
2025-09-12 22:14:34 -0300

    Aula 1 js faculdade

---
+++ Projetos/aulasFaculdade/aula1/src/filmes.js
@@ -0,0 +1,7 @@
+export default [
+    { nome: "Poderoso chefão", genero: "Drama/Ação", ano: "1972" },
+    { nome: "As branquelas", genero: "Comédia", ano: "2004" },
+    { nome: "Sexta feira 13", genero: "Terror", ano: "1980" },
+    { nome: "Matrix", genero: "Ficção Científica", ano: "1999" },
+    { nome: "Kong fu panda", genero: "Animação", ano: "2008" },
+]
---
+++ Projetos/aulasFaculdade/aula1/src/index.js
@@ -0,0 +1,23 @@
+// import FILMES from "./filmes.js"
+import { drawFilmes, create, save, remove } from "./list-filmes.js"
+
+console.log("Hello");
+
+function start() {
+    //? Reset filmes
+    //* localStorage.setItem('filmes', JSON.stringify(FILMES))
+
+    const filmes = JSON.parse(
+        localStorage.getItem('filmes')
+    )
+
+    drawFilmes(filmes);
+
+    document.getElementById('novo').addEventListener("click", create)
+    document.getElementById('salvar').addEventListener("click", save)
+    document.getElementById('remover').addEventListener("click", remove)
+
+}
+
+
+start();
